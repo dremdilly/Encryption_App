3df2045fd2d32123e3c538187a0a766f3b1f6f41
dremdilly <73487673+dremdilly@users.noreply.github.com>
2021-07-14 16:29:52 +0600

    Saving of file's path and his name

--- app/src/main/java/com/daurenbek/encryptionapp/MainActivity.kt
+++ app/src/main/java/com/daurenbek/encryptionapp/MainActivity.kt
@@ -4,15 +4,21 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.OpenableColumns
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.daurenbek.encryptionapp.databinding.ActivityMainBinding
+import java.io.File
+import java.io.FileInputStream
 
 class MainActivity : AppCompatActivity() {
-    private val chooser = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        startChooser(result, 111)
-    }
+    private val fileChooser =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            chooseFile(result)
+        }
     lateinit var binding: ActivityMainBinding
     lateinit var filepath: Uri
 
@@ -36,14 +42,26 @@ class MainActivity : AppCompatActivity() {
     private fun openFileChooser() {
         val intent = Intent(Intent.ACTION_GET_CONTENT)
         intent.type = "*/*"
-        chooser.launch(Intent.createChooser(intent, "Choose File"))
-
+        fileChooser.launch(intent)
     }
 
-    private fun startChooser(result: ActivityResult, resultCode: Int) {
-        if (resultCode == Activity.RESULT_OK) {
+    private fun chooseFile(result: ActivityResult) {
+        if (result.resultCode == Activity.RESULT_OK) {
             val data: Intent? = result.data
-            filepath = data?.data!!
+            val filepath = data?.data!!
+            var fileName: String = ""
+            filepath.let { uri ->
+                contentResolver.query(uri, null, null, null, null)
+            }?.use { cursor ->
+                val nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                cursor.moveToFirst()
+                fileName = cursor.getString(nameIndex)
+            }
+            Toast.makeText(applicationContext, fileName, Toast.LENGTH_SHORT).show()
+            val folder =
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+            val myFile = File(folder, fileName)
+            val fis = FileInputStream(myFile)
         }
     }
 }
